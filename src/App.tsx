b63c20f4a78624a2c42b9ef653691cc33410589a
Pias009 <pvs178380@gmail.com>
2026-03-08 14:24:59 +0600

    project add test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/glass" element={<AdminPanel />} />
       </Routes>
     </Router>
   );
